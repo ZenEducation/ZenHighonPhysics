941b4f7cb906c266e6ddecba356ba15378097afd
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-25 12:15:09 +0530

    Updates to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,9 @@
 <template>
-  <div>
-    <div class="py-4 flex justify-evenly bg-gray-900 text-white">
-      <NuxtLink to="/">Index Page</NuxtLink>
-      <NuxtLink to="/fullpage"> Full Page </NuxtLink>
-      <NuxtLink to="/fullscreen"> Full Screen </NuxtLink>
-    </div>
-  </div>
-
-  <div class="py-4 flex justify-evenly bg-gray-900 text-white">
-    <NuxtLink to="/">Index Page</NuxtLink>
-    <NuxtLink to="/nefaLanding"> Nefa Landing </NuxtLink>
-    <NuxtLink to="/testimonials"> Testimonials Grid Section</NuxtLink>
-  </div>
+  <IndexPageFSS />
 </template>
 
-<script>
-import { reactive, toRefs } from "vue";
-export default {
-  setup() {
-    const state = reactive({
-      count: 0,
-    });
-    return {
-      ...toRefs(state),
-    };
-  },
-};
+<script setup>
+import IndexPageFSS from "~~/components/IndexPageFSS.vue";
 </script>
 
 <style lang="scss" scoped></style>
